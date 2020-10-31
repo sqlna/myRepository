a360d4d0d9c9543bdd17afebd0fada59ff6358be
sql <2423871866@qq.com>
2020-10-31 10:17:04 +0800

    第一次提交的内容

---
+++ User.java
@@ -0,0 +1,17 @@
+package com.offcn.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private Integer id;
+    private Integer age;
+    private String name;
+
+
+}
